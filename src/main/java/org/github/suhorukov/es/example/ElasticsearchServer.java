5d9f8c33f9ae620558fd0a0d605feafb394e89da
igor-suhorukov <igor.suhorukov@gmail.com>
2015-11-19 00:18:15 +0300

    Initial commit

---
+++ src/main/java/org/github/suhorukov/es/example/ElasticsearchServer.java
@@ -0,0 +1,29 @@
+package org.github.suhorukov.es.example;
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ */
+public class ElasticsearchServer {
+    public static void main(String[] args) throws Exception{
+
+        int durationInSeconds = args.length == 0 ? 360 : Integer.parseInt(args[0]);
+
+        String template;
+        try(InputStream templateStream = new URL("https://raw.githubusercontent.com/logstash-plugins/logstash-output-elasticsearch/master/lib/logstash/outputs/elasticsearch/elasticsearch-template.json").openStream()){
+            template = new String(sun.misc.IOUtils.readFully(templateStream, -1, true));
+        }
+
+        Node elasticsearchServer = NodeBuilder.nodeBuilder().settings(ImmutableSettings.settingsBuilder().put("http.cors.enabled","true")).clusterName("elasticsearchServer").data(true).build();
+        Node node = elasticsearchServer.start();
+        node.client().admin().indices().preparePutTemplate("logstash").setSource(template).get();
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(durationInSeconds));
+    }
+}
